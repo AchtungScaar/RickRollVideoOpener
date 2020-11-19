18b39795017a068636e402038e246986f65c14a8
AchtungScaar <74726063+AchtungScaar@users.noreply.github.com>
2020-11-19 15:38:38 +0000

    Added a file
    :)

---
+++ rickrollerskidded.py
@@ -0,0 +1,6 @@
+import os
+import time
+while 1 == 1:
+    print ("I skidded this cause some kid told me not to :)")
+    time.sleep(1)
+    os.system("start https://www.youtube.com/watch?v=oHg5SJYRHA0")
